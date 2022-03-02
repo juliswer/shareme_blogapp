3ea7bf1d05b4068f5edfac95afffbbc87f4ffbb5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-02 16:42:35 -0300

    uploading schemas

--- shareme_backend/schemas/schema.js
+++ shareme_backend/schemas/schema.js
@@ -5,6 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import user from './user'
+import pin from './pin'
+import comment from './comment'
+import save from './save'
+import postedBy from './postedBy'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
