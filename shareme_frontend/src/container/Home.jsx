8c46d4889e4f425642784f2b8ae16c860ad99a70
Julian Swerdlin <julianswer36@gmail.com>
2022-03-05 12:00:59 -0300

    uploading the home page

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           <img src={logo} alt="logo" className="w-28" />
         </Link>    
         <Link to={`user-profile/${user?._id}`}>
-          <img src={logo} alt="logo" className="w-28" />
+          <img src={user?.image} alt="logo" className="w-28" />
         </Link>    
       </div>
     </div>
