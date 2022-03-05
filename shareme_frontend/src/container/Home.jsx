629c88ccd3fb0d6ff3a13912a0698170f5ae3622
Julian Swerdlin <julianswer36@gmail.com>
2022-03-05 11:31:56 -0300

    uploading the home page

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -3,6 +3,10 @@ import {HiMenu} from 'react-icons/hi';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import {Link, Route, Routes} from 'react-router-dom';
 
+import {Sidebar, UserProfile} from '../components';
+import {client} from '../client';
+import logo from '../assets/logo.png'
+
 const Home = () => {
   return (
     <div>Home</div>
