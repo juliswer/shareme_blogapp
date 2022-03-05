d8b245a831561cea19e2a4d9fdc6794b5f78562c
Julian Swerdlin <julianswer36@gmail.com>
2022-03-05 11:22:59 -0300

    fixing a cors error with sanity

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -5,7 +5,11 @@ import {FcGoogle} from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 
+import {client} from '../client'
+
 const Login = () => {
+
+  const navigate = useNavigate();
 
   const responseGoogle = (response) => {
     localStorage.setItem('user', JSON.stringify(response.profileObj));
@@ -18,6 +22,11 @@ const Login = () => {
       userName: name,
       image: imageUrl
     }
+
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate('/', {replace: true})
+      })
   }
 
   return (
