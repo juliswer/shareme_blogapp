8a60a06f2900996d67b8ca3b0867c8d6715d899c
Julian Swerdlin <julianswer36@gmail.com>
2022-03-05 11:03:43 -0300

    uploading the login page

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -6,6 +6,11 @@ import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 
 const Login = () => {
+
+  const responseGoogle = (response) => {
+
+  }
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -26,15 +31,20 @@ const Login = () => {
 
           <div className="shadow-2xl">
             <GoogleLogin 
-              clientId=''
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
               render={(renderProps) => (
                 <button
                   type="button"
                   className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
                 >
                   <FcGoogle className="mr-4" /> Sign in with Google
                 </button>
               )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
             />
           </div>
         </div>
