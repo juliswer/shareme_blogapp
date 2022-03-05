715a7634c36651565b92bfd8bab236c9c7c7c73f
Julian Swerdlin <julianswer36@gmail.com>
2022-03-05 10:55:32 -0300

    uploading the login page

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -18,6 +18,27 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin 
+              clientId=''
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   )
