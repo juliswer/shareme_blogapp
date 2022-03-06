04c319101d05581576792bf58f09e03054c3a9b8
Julian Swerdlin <julianswer36@gmail.com>
2022-03-06 11:15:59 -0300

    updating the feed

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -6,6 +6,11 @@ import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    if(loading) return <Spinner message="We are adding new ideas to your feed!" />
+
   return (
     <div>Feed</div>
   )
