d0d156e3ba36210a94b45e55fab09f93519f1ac1
Julian Swerdlin <julianswer36@gmail.com>
2022-03-06 17:45:43 -0300

    pin detail page

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -16,6 +16,29 @@ const PinDetail = ({user}) => {
   const [addingComment, setAddingComment] = useState(false);
   const {pinId} = useParams();
 
+  const addComment = () => {
+    if(comment) {
+      setAddingComment(true);
+      client
+        .patch(pinId)
+        .setIfMissing({comments: []})
+        .insert('after', 'comments[-1]', [{
+          comment,
+          _key: uuidv4(),
+          postedBy: {
+            _type: 'postedBy',
+            _ref: user._id
+          }
+        }])
+          .commit()
+          .then(() => {
+            fetchPinDetails();
+            setComment('');
+            setAddingComment(false);
+          })
+    }
+  }
+
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
 
@@ -95,6 +118,29 @@ const PinDetail = ({user}) => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="flex flex-wrap mt-6 gap-3">
+          <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+            <img
+              className="w-10 h-10 rounded-full cursor-pointer"
+              src={pinDetail.postedBy?.image}
+              alt="user-profile"
+            />
+          </Link>
+          <input
+            className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+            type="text"
+            placeholder="Add a comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+            onClick={addComment}
+          >
+            {addingComment ? 'Posting the comment...' : 'Posted'}
+          </button>
         </div>
       </div>
     </div>
