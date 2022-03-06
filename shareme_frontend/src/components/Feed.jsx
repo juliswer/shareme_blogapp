fd914d1a53c87da5563b42ea8bb3c1e803d69eb5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-06 11:26:48 -0300

    uploading the queries

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -2,12 +2,31 @@ import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
 import {client} from '../client';
+import { searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
 
+    const [pins, setPins] = useState(null);
     const [loading, setLoading] = useState(false);
+    const {categoryId} = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        if(categoryId) {
+            const query = searchQuery(categoryId);
+
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data)
+                    setLoading(false);
+                })
+        } else {
+
+        }
+
+    }, [categoryId]);
 
     if(loading) return <Spinner message="We are adding new ideas to your feed!" />
 
